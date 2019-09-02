81af73f35925d3d68661f3ca69237e0ed3542769
hiro0218 <hiro0218@gmail.com>
2019-09-03 08:55:40 +0900

    fix: event optimization

--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
 export default class Accordion {
   constructor(elDefault, interlocking = false) {
+    this.event = 'ontouchstart' in window ? 'touchstart' : 'click';
+
     this.elAccordions = elDefault || document.querySelectorAll('[data-accordion]');
     this.interlocking = interlocking;
 
     Array.from(this.elAccordions, elAccordion => {
-      this.eventHandler(elAccordion);
+      const elHeader = elAccordion.querySelector('[data-accordion-header]');
+
+      this.eventHandler(elHeader);
     });
   }
 
-  eventHandler(elAccordion) {
-    const elHeader = elAccordion.querySelector('[data-accordion-header]');
+  eventHandler(elHeader) {
+    elHeader.addEventListener(this.event, e => {
+      e.preventDefault();
 
-    elHeader.addEventListener('click', () => {
+      const nextElement = e.target.nextElementSibling;
+      nextElement.classList.toggle('is-animated');
+
       this.toggleAriaExpanded(elHeader);
 
       // 連動式
@@ -19,13 +26,16 @@ export default class Accordion {
         // インスタンス時に取得したアコーディオン要素を精査する
         Array.from(this.elAccordions, el => {
           // クリックした要素だったら抜ける
-          if (elAccordion === el) return;
+          if (e.target.parentNode === el) return;
 
           // アコーディオンのヘッダーを取得
           const elAloneHeader = el.querySelector('[data-accordion-header]');
           if (elAloneHeader.getAttribute('aria-expanded') === 'true') {
             this.toggleAriaExpanded(elAloneHeader);
           }
+
+          const nextElement = elAloneHeader.nextElementSibling;
+          nextElement.classList.toggle('is-animated');
         });
       }
     });
