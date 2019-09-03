c639e4b34437ec8ec6c21037128ec66244138999
hiro <hiro0218@gmail.com>
2019-09-03 23:23:44 +0900

    fix: array.from -> for

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 export default class Accordion {
   constructor(elDefault, interlocking = false) {
-    this.event = 'ontouchstart' in window ? 'touchstart' : 'click';
+    this.event = "ontouchstart" in window ? "touchstart" : "click";
 
-    this.elAccordions = elDefault || document.querySelectorAll('[data-accordion]');
+    this.elAccordions =
+      elDefault || document.querySelectorAll("[data-accordion]");
     this.interlocking = interlocking;
 
-    Array.from(this.elAccordions, elAccordion => {
-      const elHeader = elAccordion.querySelector('[data-accordion-header]');
-
+    for (let i = 0; i < this.elAccordions.length; i++) {
+      const elHeader = this.elAccordions[i].querySelector(
+        "[data-accordion-header]"
+      );
       this.eventHandler(elHeader);
-    });
+    }
   }
 
   eventHandler(elHeader) {
@@ -17,31 +19,36 @@ export default class Accordion {
       e.preventDefault();
 
       const nextElement = e.target.nextElementSibling;
-      nextElement.classList.toggle('is-animated');
+      nextElement.classList.toggle("is-animated");
 
       this.toggleAriaExpanded(elHeader);
 
       // 連動式
       if (this.interlocking) {
         // インスタンス時に取得したアコーディオン要素を精査する
-        Array.from(this.elAccordions, el => {
+        for (let i = 0; i < this.elAccordions.length; i++) {
           // クリックした要素だったら抜ける
-          if (e.target.parentNode === el) return;
+          if (e.target.parentNode === this.elAccordions[i]) continue;
 
           // アコーディオンのヘッダーを取得
-          const elAloneHeader = el.querySelector('[data-accordion-header]');
-          if (elAloneHeader.getAttribute('aria-expanded') === 'true') {
+          const elAloneHeader = this.elAccordions[i].querySelector(
+            "[data-accordion-header]"
+          );
+          const nextElement = elAloneHeader.nextElementSibling;
+          nextElement.classList.toggle("is-animated");
+
+          if (elAloneHeader.getAttribute("aria-expanded") === "true") {
             this.toggleAriaExpanded(elAloneHeader);
           }
-
-          const nextElement = elAloneHeader.nextElementSibling;
-          nextElement.classList.toggle('is-animated');
-        });
+        }
       }
     });
   }
 
   toggleAriaExpanded(element) {
-    element.setAttribute('aria-expanded', element.getAttribute('aria-expanded') === 'true' ? 'false' : 'true');
+    element.setAttribute(
+      "aria-expanded",
+      element.getAttribute("aria-expanded") === "true" ? "false" : "true"
+    );
   }
 }
