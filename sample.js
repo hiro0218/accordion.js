1f4e04223018e27cb39d7b9a888c16d413dde9e1
hiro0218 <hiro0218@gmail.com>
2019-09-04 00:36:41 +0900

    add: ローカル確認環境を構築

--- sample.js
+++ sample.js
@@ -1,9 +1,6 @@
-import "./src/index.scss";
-import Accordion from "./src/index";
+import './src/index.scss';
+import Accordion from './src/index';
 
-new Accordion(document.querySelectorAll(".accordion-default [data-accordion]"));
+new Accordion(document.querySelectorAll('.accordion-default [data-accordion]'));
 
-new Accordion(
-  document.querySelectorAll(".accordion-interlocking [data-accordion]"),
-  true
-);
+new Accordion(document.querySelectorAll('.accordion-interlocking [data-accordion]'), true);
